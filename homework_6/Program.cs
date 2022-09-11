0d71daf72afabf7d8787ebfc69a31b2caacb611e
BolotovA <BolotovA@fizpribor.ru>
2022-09-11 22:57:27 +0300

    Вторая задача

--- homework_6/Program.cs
+++ homework_6/Program.cs
@@ -49,4 +49,30 @@ findPositiv(createArray(lenArr));
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 //            заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/*
+
+void findPoint( double b1, double k1, double b2, double k2 )
+{
+    if ( k1 == k2 && b1 != b2 )
+    {
+        Console.WriteLine("Параллельны");
+    }
+    else if ( k1 == k2 && b1 == b2 )
+    {
+        Console.WriteLine("Совпадают");
+    }
+    else
+    {
+        double x = ( b2 - b1 ) / ( k1 - k2 );
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"({x}, {y})");
+    }
+}
+
+findPoint(b1: 2,
+          k1: 5,
+          b2: 4,
+          k2: 9);
+
+*/
